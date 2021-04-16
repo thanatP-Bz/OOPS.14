738cd64789d0656461341172896e665faf2a6a8d
Thanat <bztzn512@gmail.com>
2021-04-16 09:53:35 -0500

    CodingChallege3

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ const Mandy = new CalHeight('Mandy', 180);
 Bree.deHeight(); */
 
 //classes ES6
-class PersonInfo {
+/* class PersonInfo {
       constructor(fullName, birthYear) {
             this.fullName = fullName;
             this.birthYear = birthYear;
@@ -148,7 +148,7 @@ wicky.fristName = 'Wicky';
 wicky.birthYear = 1992;
 wicky.calAge();
 
-console.log(wicky.__proto__ === PersonProto);
+console.log(wicky.__proto__ === PersonProto) */;
 
 /* personInfo.prototype.greet = function() {
       console.log(`Hello ${this.fristName}`);
@@ -209,7 +209,7 @@ const colin = new calpoints('Colin', 25);
 console.log(colin.extra); */
 
 ////coding challege 2
-class Car {
+/* class Car {
       constructor (carName, speed) {
             this.speed = speed;
             this.carName = carName;
@@ -229,3 +229,102 @@ const carOne = new Car('Ford', 120);
 console.log(carOne.speedUs);
 carOne.speedUs = 50;
 console.log(carOne);
+ */
+/////////////////////////////
+/////inheritance between classes and contructor
+/* 
+class PersonInfo {
+      constructor (firstName, birthYear) {
+            this.fullName = firstName;
+            this.birthYear = birthYear;
+      }
+      calAge() {
+            this.birthYear = 2021 - this.birthYear;
+            console.log(`${this.fristName} is ${this.birthYear} years old`);
+       }
+};
+
+class Student {
+      constructor (firstName, birthYear, course) {
+            PersonInfo.call(this, firstName, birthYear);//use call method to call the function in order to not repeat yourself
+            this.course = course;
+      }
+
+      geeting() {
+            return `Hello everyone my name is ${this.firstName}`
+      }
+};
+
+const redmond = new Student('Red', 1993, 'Sciences');
+redmond.geeting(); */
+/////////////////////////////
+
+const Person = function (firstName, birthYear) {
+      this.firstName = firstName;
+      this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+      console.log(2021 - this.birthYear);
+}
+
+const NewStudent = function (firstName, birthYear, course) {
+      Person.call(this, firstName, birthYear);
+      this.course = course;
+};
+
+NewStudent.prototype = Object.create(Person.prototype); //use Object create to connect and share behaviour with the parent
+
+NewStudent.prototype.introduce = function() {
+      console.log(`hello my name is ${this.firstName}`);
+};
+
+
+const mike = new NewStudent('Mike', 1993, 'Science');
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+
+NewStudent.prototype.constructor = NewStudent;
+console.dir(NewStudent.prototype.constructor);
+///////////////////////////////////////////////
+////////////coding challege 3
+
+const Car2 = function (make, speed) {
+      this.make = make;
+      this.speed = speed;
+};
+
+Car2.prototype.accelerate = function () {
+      this.speed += 10;
+      console.log(`${this.make} is going to ${this.speed}`);
+};
+
+Car2.prototype.brake = function () {
+      this.speed -= 5;
+      console.log(`${this.make} is going at ${this.speed}`);
+};
+
+const EV = function (make, speed, charge) {
+      Car2.call(this,make, speed);
+      this.charge = charge;
+};
+//link to prototypes
+EV.prototype = Object.create(Car2.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+      this.charge = chargeTo;
+}
+
+EV.prototype.accelerate = function () {
+      this.speed += 20;
+      this.charge --;
+      console.log(`${this.make} is going at ${this.speed} km/h with a charge of ${this.charge}`);
+}
+
+const tesla = new EV ('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+tesla.accelerate();
